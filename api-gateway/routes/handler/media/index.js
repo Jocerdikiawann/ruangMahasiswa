c9ee73da889c2de976dbc734c456030d4e2fec9b
Jocerdikiawann <cocokcocok33@gmail.com>
2021-08-27 02:06:30 +0700

    api adapter media delete

--- api-gateway/routes/handler/media/index.js
+++ api-gateway/routes/handler/media/index.js
@@ -1,4 +1,8 @@
 const create = require("./create");
+const getAll = require("./getAll");
+const destroy = require("./destroy");
 module.exports = {
   create,
+  getAll,
+  destroy,
 };
--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const mediaHandler = require("./handler/media");
 /* GET users listing. */
 router.post("/", mediaHandler.create);
+router.get("/", mediaHandler.getAll);
+router.delete("/:id", mediaHandler.destroy);
 
 module.exports = router;
