3d55863a17e663a214ae4975f0d0a41d28feab3b
Jocerdikiawann <cocokcocok33@gmail.com>
2021-10-17 02:37:09 +0700

    feat: menambahkan handler user register

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userHandler = require("./handler/users");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/register", userHandler.register);
 
 module.exports = router;
