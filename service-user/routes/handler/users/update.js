4b2d1b023bfaac714eda0b7cbda8e682688f0096
Jocerdikiawann <cocokcocok33@gmail.com>
2021-10-17 11:36:31 +0700

    feat: menambahkan handler user update dan get user by id

---
+++ service-user/routes/handler/users/update.js
@@ -0,0 +1,66 @@
+const bycript = require("bcrypt");
+const { User } = require("../../../models");
+const Validator = require("fastest-validator");
+const v = new Validator();
+
+module.exports = async (req, res) => {
+  const schema = {
+    name: "string|empty:false",
+    email: "email|empty:false",
+    password: "string|min:6",
+    profession: "string|optional",
+    avatar: "string|optional",
+  };
+
+  const validate = v.validate(req.body, schema);
+
+  if (validate.length) {
+    res.status(400).json({
+      status: "error",
+      message: validate,
+    });
+  }
+
+  const id = req.params.id;
+  const user = await User.findByPk(id);
+  if (!user) {
+    res.status(400).json({
+      status: "error",
+      message: "user not found",
+    });
+  }
+
+  const email = req.body.email;
+  if (email) {
+    const checkEmail = await User.findOne({
+      where: { email },
+    });
+    if (checkEmail && email !== user.email) {
+      res.status(409).json({
+        status: "error",
+        message: "email already exist",
+      });
+    }
+  }
+
+  const password = await bycript.hash(req.body.password, 10);
+  const { name, avatar, profession } = req.body;
+  await user.update({
+    email,
+    password,
+    name,
+    profession,
+    avatar,
+  });
+
+  return res.json({
+    status: "success",
+    data: {
+      id: user.id,
+      name,
+      email,
+      profession,
+      avatar,
+    },
+  });
+};
